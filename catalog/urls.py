e7962b2acf387a512bc05ebc908db89300451a4f
Алексей <abe26@yandex.ru>
2026-01-12 10:52:15 +0300

    Djangoproect

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import ProductDetailView, HomeView, ContactsView
+from . import views
 
-app_name = 'catalog'  # пространство имён (важно для {% url %} в шаблонах)
+app_name = 'catalog'
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
-    path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('product_list/', views.product_list, name='product_list'),
+    path('<int:pk>/', views.product_detail, name='product_detail'),
+    path('create/', views.product_create, name='product_create'),
+    path('<int:pk>/update/', views.product_update, name='product_update'),
+    path('<int:pk>/delete/', views.product_delete, name='product_delete'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,22 +1,43 @@
-from django.views.generic import DetailView, ListView, TemplateView
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from .models import Product
+from .forms import ProductForm
 
+def product_list(request):
+    products = Product.objects.all().select_related('category')
+    return render(request, 'catalog/list.html', {'products': products})
 
-class ProductDetailView(DetailView):
-    model = Product
-    template_name = 'catalog/product_detail.html'
-    context_object_name = 'product'
-    pk_url_kwarg = 'pk'  # имя параметра в URL
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'catalog/product_detail.html', {'product': product})
 
+def product_create(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Продукт успешно создан!')
+            return redirect('catalog:product_list')
+    else:
+        form = ProductForm()
+    return render(request, 'catalog/create.html', {'form': form})
 
-class HomeView(ListView):
-    model = Product
-    template_name = 'catalog/home.html'
-    context_object_name = 'products'
+def product_update(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Продукт обновлён!')
+            return redirect('catalog:product_detail', pk=product.pk)
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'catalog/update.html', {'form': form, 'product': product})
 
-    def get_queryset(self):
-        return Product.objects.all()[:10]  # как в исходном FBV
-
-
-class ContactsView(TemplateView):
-    template_name = 'catalog/contacts.html'
+def product_delete(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        product.delete()
+        messages.success(request, 'Продукт удалён!')
+        return redirect('catalog:product_list')
+    return render(request, 'catalog/delete.html', {'product': product})
